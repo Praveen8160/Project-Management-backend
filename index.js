d3de3d9c5e10202266f318924dadf0afd740518b
Praveen8160 <praveenp8160@gmail.com>
2024-07-19 16:56:21 +0530

    set bugs

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://project-management-frontend-21t1.onrender.com",
     credentials: true, // If you are sending cookies or authorization headers
   })
 );
